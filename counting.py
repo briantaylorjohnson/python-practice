bbdb4b38a02e4c63adc5da65e5bcfbca9516852e
Brian Taylor Johnson <briantaylorjohnson@gmail.com>
2020-04-04 00:50:48 -0400

    Python user input and while loop practice on 4/3

---
+++ counting.py
@@ -0,0 +1,13 @@
+### Chapter 7
+
+## The While Loop
+
+# While loop will run until a certain condition is TRUE
+
+current_num = 1
+
+while current_num <= 5: # Loop runs while the current_num variable is less or equal to 5
+    
+    print(current_num) # Outputs the value for current_num variable
+
+    current_num +=1 # Increments the current_num variable by 1
---
+++ rollercoaster.py
@@ -0,0 +1,29 @@
+### Chapter 7
+
+## Using the int() function
+
+height = input("How tall are you in inches? ")
+
+height = int(height)
+
+if height >= 48:
+    print("\nYou are tall enough to ride!")
+
+else:
+    print("\nYou'll be able to ride when you're a little older.")
+
+
+## The Modulo Operator
+
+# Modulo operator returns the remainder of dividing two numbers
+modulo = 4 % 3
+print(f"\n{modulo}") # 1
+
+modulo = 5 % 3
+print(f"\n{modulo}") # 2
+
+modulo = 6 % 3
+print(f"\n{modulo}") # 
+
+modulo = 7 % 3
+print(f"\n{modulo}") # 1
