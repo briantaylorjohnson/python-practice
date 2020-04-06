6c35a4ea558ad3579cc13d63b6c0cb46e02762b6
Brian Taylor Johnson <briantaylorjohnson@gmail.com>
2020-04-06 15:51:38 -0400

    Some more Python practice with files and working with them...

--- write_message.py
+++ write_message.py
@@ -3,3 +3,23 @@
 ## Writing to a File
 
 # Writing to an Empty File
+
+file_name = 'programming.txt'
+
+""" 'w' , 'r' , and 'a' are 'write', 'read only', and 'append' respectively; there is also 'r+' which allows reading and writing to the file. """
+with open(file_name, 'w') as file_object:
+    file_object.write('I love programming.')
+
+
+# Writing Multiple Lines
+
+with open(file_name, 'w') as file_object:
+    file_object.write('I love programming.\n')
+    file_object.write('I love creating new games.\n')
+
+
+# Appending to a File
+
+with open(file_name, 'a') as file_object:
+    file_object.write('I also love finding meaning in large datasets.\n')
+    file_object.write('I love creating apps that can run in a browser.\n')
